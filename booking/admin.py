917f9213c425918ee40e505c61c67c31d60f1c95
Lawrence Peters <lawrence.peters@gmail.com>
2018-04-10 16:12:28 +0000

    Set up the database framework

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,6 @@
 
 from django.contrib import admin
 
-from .models import Room
+from .models import roomInfo
 
-admin.site.register(Room)
+admin.site.register(roomInfo)
--- booking/models.py
+++ booking/models.py
@@ -3,7 +3,36 @@
 from django.db import models
 
 
-class Room(models.Model):
+class roomInfo(models.Model):
+    roomID = models.AutoField(primary_key=True)
     roomName = models.CharField(max_length=50)
     roomDescription = models.CharField(max_length=200)
     roomCapacity = models.IntegerField()
+
+class facilities(models.Model):
+    facilityID = models.AutoField(primary_key=True)
+    facilityName = models.CharField(max_length=50)
+    facilityDescription = models.CharField(max_length=200)
+
+class roomFacilities(models.Model):
+    roomID = models.ForeignKey(roomInfo, primary_key=True)
+    facilityID = models.ForeignKey(facilities)
+
+class periods(models.Model):
+    periodID = models.AutoField(primary_key=True)
+    periodName = models.CharField(max_length=50)
+    startTime = models.TimeField(auto_now=False)
+    endTime = models.TimeField(auto_now=False)
+
+class roomAvaliability(models.Model):
+    date = models.DateField(auto_now=False)
+    periodID = models.ForeignKey(periods)
+
+class bookingRecord(models.Model):
+    date = models.DateField(auto_now=False)
+    periodID = models.ForeignKey(periods)
+
+class bookingHistory(models.Model):
+    date = models.DateField(auto_now=False)
+    periodID = models.ForeignKey(periods)
+    timestamp = models.TimeField(auto_now_add=True)
